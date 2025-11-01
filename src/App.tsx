5f68659b6b83984071346945f369ef41bf606aa0
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2025-11-01 12:01:25 -0400

    feat(styles): implement dark mode support and apply global base styles     - Configure Tailwind CSS to enable dark mode switching using the 'class' strategy     - Define global base styles in  to manage theme-dependent background and text colors for the body element     - Apply smooth color transitions and set the default font family to 'Inter'     - Remove placeholder styles and temporary elements from  to utilize the new global theme settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 function App() {
   return (
     <>
-      <header className="bg-[rgba(71,210,241,0.735)]">
+      <header>
         <h1 className="text-2xl text-center">Atmos - Aplicación Meteorológica</h1>
       </header>
-      <main className="flex flex-col justify-center items-center min-h-screen w-full gap-4 bg-gray-300">
-        <p>Aquí iran los formularios</p>
-      </main>
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 const config = {
+  darkMode: 'class',
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
